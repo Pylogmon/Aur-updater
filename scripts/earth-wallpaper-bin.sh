fbfe24ef2ea2794efc252cb756d71021e292a3f5
Pylogmon <pylogmon@outlook.com>
2023-02-16 11:05:36 +0800

    add earth-wallpaper-bin

---
+++ scripts/earth-wallpaper-bin.sh
@@ -0,0 +1,8 @@
+cd earth-wallpaper-bin
+
+ver=$(curl -s https://api.github.com/repos/Pylogmon/earth-wallpaper/releases/latest | jq '.tag_name'|tr -d 'v"')
+sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
+sudo -u builder  updpkgsums
+
+rm earth-wallpaper-${ver}.tar.gz
+cd -
