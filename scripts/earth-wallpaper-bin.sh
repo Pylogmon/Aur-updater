e607d39829a61e507d7c3a41b360249a68b3f9e8
Pylogmon <pylogmon@outlook.com>
2023-02-16 11:09:42 +0800

    fix

--- scripts/earth-wallpaper-bin.sh
+++ scripts/earth-wallpaper-bin.sh
@@ -1,6 +1,6 @@
 cd earth-wallpaper-bin
 
-ver=$(curl -s https://api.github.com/repos/Pylogmon/earth-wallpaper/releases/latest | jq '.tag_name'|tr -d 'v"')
+ver=$(curl -s https://api.github.com/repos/Pylogmon/earth_wallpaper/releases/latest | jq '.tag_name'|tr -d 'v"')
 sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sudo -u builder  updpkgsums
 
