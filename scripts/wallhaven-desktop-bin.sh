3e6657397b56693f3ae22a9af2ea779024131284
派了个萌 <59004461+Pylogmon@users.noreply.github.com>
2023-02-17 22:29:45 +0800

    Update wallhaven-desktop-bin.sh

--- scripts/wallhaven-desktop-bin.sh
+++ scripts/wallhaven-desktop-bin.sh
@@ -1,6 +1,6 @@
 cd wallhaven-desktop-bin
 
-ver=$(curl -s https://api.github.com/repos/Pylogmon/wallhaven-desktop/releases/latest | jq '.tag_name'|tr -d 'v"')
+ver=$(curl -s https://api.github.com/repos/Pylogmon/wallhaven-desktop/releases/latest | jq '.tag_name' | tr -d '"')
 sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sudo -u builder  updpkgsums
 
