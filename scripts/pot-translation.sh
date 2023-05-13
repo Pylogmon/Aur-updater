20c59eb0d7d4ef9329fd88db6baf8dc10fe3b66f
Pylogmon <pylogmon@outlook.com>
2023-05-13 13:14:27 +0800

    Update

--- scripts/pot-translation.sh
+++ scripts/pot-translation.sh
@@ -1,6 +1,6 @@
 cd pot-translation
 
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/Pylogmon/pot/releases/latest | jq '.tag_name'|tr -d 'v"')
+ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/pot-app/pot-desktop/releases/latest | jq '.tag_name'|tr -d 'v"')
 sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sudo -u builder  updpkgsums
 
