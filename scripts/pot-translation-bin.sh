9b48c889df6c6988c8b32952ec59f0ba59a27ef4
Pylogmon <pylogmon@outlook.com>
2023-09-09 11:29:12 +0800

    Update pot-translation-bin.sh

--- scripts/pot-translation-bin.sh
+++ scripts/pot-translation-bin.sh
@@ -1,7 +1,7 @@
 cd pot-translation-bin
 
 ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/pot-app/pot-desktop/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/_pkgver=.*/_pkgver=${ver}/" PKGBUILD
+sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sudo -u builder  updpkgsums
 
 rm pot-${ver}-x86_64.deb
