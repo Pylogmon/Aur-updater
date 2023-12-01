8059033d1ff0db908b51655d4b49db3d7090fbb8
Pylogmon <pylogmon@outlook.com>
2023-12-01 13:57:00 +0800

    Update clash-verge-rev-bin.sh

--- scripts/clash-verge-rev-bin.sh
+++ scripts/clash-verge-rev-bin.sh
@@ -1,6 +1,6 @@
 cd clash-verge-rev-bin
 
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/wonfen/clash-verge-rev/releases/latest | jq '.tag_name'|tr -d 'v"')
+ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/clash-verge-rev/clash-verge-rev/releases/latest | jq '.tag_name'|tr -d 'v"')
 sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sudo -u builder  updpkgsums
 
