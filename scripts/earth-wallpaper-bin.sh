05e18a9c61d127b412b8e425ac053b41e8aaaad4
Pylogmon <pylogmon@outlook.com>
2024-03-22 09:06:14 +0800

    update

--- scripts/earth-wallpaper-bin.sh
+++
@@ -1,8 +0,0 @@
-cd earth-wallpaper-bin
-
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/Pylogmon/earth_wallpaper/releases/latest | jq '.tag_name' | tr -d '"')
-sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
-sudo -u builder  updpkgsums
-
-rm earth-wallpaper-${ver}.tar.gz
-cd -
--- scripts/wallhaven-desktop-bin.sh
+++
@@ -1,8 +0,0 @@
-cd wallhaven-desktop-bin
-
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/Pylogmon/wallhaven-desktop/releases/latest | jq '.tag_name' | tr -d '"')
-sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
-sudo -u builder  updpkgsums
-
-rm wallhaven-desktop-${ver}-x86_64.deb
-cd -
