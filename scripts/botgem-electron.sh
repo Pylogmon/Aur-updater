2fe0e4332cc6ed500b4ff742438cd3db0e68db0d
Pylogmon <pylogmon@outlook.com>
2024-10-29 17:03:55 +0800

    update

--- scripts/botgem-electron.sh
+++
@@ -1,7 +0,0 @@
-cd botgem-electron
-
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/gaodeng/ama-discussions/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
-sudo -u builder  updpkgsums
-
-cd -
--- scripts/clash-verge-rev-alpha-bin.sh
+++
@@ -1,6 +0,0 @@
-cd clash-verge-rev-alpha-bin
-
-ver=$(curl -H "Authorization: Bearer ${INPUT_TOKEN}" -s https://api.github.com/repos/clash-verge-rev/clash-verge-rev/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
-
-cd -
